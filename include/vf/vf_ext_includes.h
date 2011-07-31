90541deb35873e1c01fb7948cd8295f6b6d2565d
Patrick Dehne <p4tr3ck@gmail.com>
2011-07-31 13:31:02 +0000

    Intrinsics are only available on windows at the moment

--- include/vf/vf_ext_includes.h
+++ include/vf/vf_ext_includes.h
@@ -21,8 +21,9 @@ struct sqlite3_stmt;
 
 // When this causes an error (compiling on iOs or android?), we
 // will need to add some stuff to detect if intrinsics are available
+#ifdef _MSC_VER
 #include <xmmintrin.h>
 #define VF_HAVE_SIMD_INTRINSICS 1
-
 #endif
 
+#endif
