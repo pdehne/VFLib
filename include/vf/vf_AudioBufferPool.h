82306e7bca32d772552663f208fc5a7dc1e1d4b5
Patrick Dehne <p4tr3ck@gmail.com>
2011-12-14 00:22:40 +0000

    Make AudioBufferPool a template class

--- include/vf/vf_AudioBufferPool.h
+++ include/vf/vf_AudioBufferPool.h
@@ -8,38 +8,112 @@
 #include "vf/vf_LeakChecked.h"
 #include "vf/vf_Mutex.h"
 
-// Manages a collection of AudioSampleBuffer that
-// persist in memory and never decrease in size.
-class AudioBufferPool : LeakChecked <AudioBufferPool>
+class AudioBufferPoolBase
 {
 public:
-  class Buffer : public VF_JUCE::AudioSampleBuffer
-  {
-  public:
-    Buffer (int numChannels,
-            int numSamples);
+    class Buffer : public VF_JUCE::AudioSampleBuffer
+    {
+    public:
+        Buffer (int numChannels,
+                int numSamples);
+        
+        void resize (int newNumChannels, int newNumSamples);
+        
+        int getNumSamplesAllocated () const;
+        
+    private:
+        int m_samplesAllocated;
+    };
+};
 
-    void resize (int newNumChannels, int newNumSamples);
-
-    int getNumSamplesAllocated () const;
-
-  private:
-    int m_samplesAllocated;
-  };
-
+// Manages a collection of AudioSampleBuffer that
+// persist in memory and never decrease in size.
+template <class BufferClass>
+class AudioBufferPoolTemplate
+: public AudioBufferPoolBase
+, public LeakChecked <AudioBufferPoolTemplate<BufferClass> >
+{
 public:
-  AudioBufferPool ();
-  ~AudioBufferPool ();
+  AudioBufferPoolTemplate ()
+  {
+    m_buffers.ensureStorageAllocated (10);
+  }
+    
+  ~AudioBufferPoolTemplate ()
+  {
+    for (int i = 0; i < m_buffers.size(); ++i)
+      delete m_buffers[i];
+  }    
 
-  Buffer* acquireBuffer (int numChannels,
-                         int numSamples);
+  BufferClass* acquireBuffer (int numChannels, int numSamples)
+  {
+    BufferClass* buffer = 0;
+    int samplesNeeded = numChannels * numSamples;
+    
+    int index = -1;
+    
+    {
+        m_mutex.enter ();
+        
+        for (int i = 0; i < m_buffers.size(); ++i)
+        {
+            BufferClass* cur = m_buffers[i];
+            
+            if (!buffer)
+            {
+                buffer = cur;
+                index = i;
+            }
+            else
+            {
+                int numSamplesAvailable = cur->getNumSamplesAllocated();
+                
+                // Use the smallest buffer which is equal or bigger than what
+                // we need. If no buffer is large enough, then we will use the
+                // largest available and increase it, to minimize memory usage.
+                if ( (numSamplesAvailable >= samplesNeeded &&
+                      numSamplesAvailable < buffer->getNumSamplesAllocated()) ||
+                    (numSamplesAvailable < samplesNeeded &&
+                     numSamplesAvailable > buffer->getNumSamplesAllocated()))
+                {
+                    buffer = cur;
+                    index = i;
+                }
+            }
+        }
+        
+        if (buffer)
+            m_buffers.remove (index);
+        
+        m_mutex.exit();
+    }
+    
+    if (buffer)
+        buffer->resize (numChannels, numSamples);
+    else
+        buffer = new BufferClass (numChannels, numSamples);
+    
+    return buffer;
+  }
 
-  void releaseBuffer (Buffer* buffer);
+  void releaseBuffer (BufferClass* buffer)
+  {
+    if (buffer)
+    {
+        m_mutex.enter ();
+        
+        m_buffers.add (buffer);
+        
+        m_mutex.exit ();
+    }
+  }    
 
 private:
   Mutex m_mutex;
-  VF_JUCE::Array <Buffer*> m_buffers;
+  VF_JUCE::Array <BufferClass*> m_buffers;
 };
+
+typedef AudioBufferPoolTemplate<AudioBufferPoolBase::Buffer> AudioBufferPool;
 
 //------------------------------------------------------------------------------
 
--- source/vf_AudioBufferPool.cpp
+++ source/vf_AudioBufferPool.cpp
@@ -8,14 +8,14 @@ BEGIN_VF_NAMESPACE
 
 #include "vf/vf_AudioBufferPool.h"
 
-AudioBufferPool::Buffer::Buffer (int numChannels,
+AudioBufferPoolBase::Buffer::Buffer (int numChannels,
                                  int numSamples)
   : AudioSampleBuffer (numChannels, numSamples)
   , m_samplesAllocated (numChannels * numSamples)
 {
 }
 
-void AudioBufferPool::Buffer::resize (int newNumChannels, int newNumSamples)
+void AudioBufferPoolBase::Buffer::resize (int newNumChannels, int newNumSamples)
 {
   int samplesAllocated = newNumChannels * newNumSamples;
 
@@ -25,85 +25,9 @@ void AudioBufferPool::Buffer::resize (int newNumChannels, int newNumSamples)
   setSize (newNumChannels, newNumSamples, false, false, true);
 }
 
-int AudioBufferPool::Buffer::getNumSamplesAllocated () const
+int AudioBufferPoolBase::Buffer::getNumSamplesAllocated () const
 {
   return m_samplesAllocated;
-}
-
-//------------------------------------------------------------------------------
-
-AudioBufferPool::AudioBufferPool()
-{
-  m_buffers.ensureStorageAllocated (10);
-}
-
-AudioBufferPool::~AudioBufferPool()
-{
-  for (int i = 0; i < m_buffers.size(); ++i)
-    delete m_buffers[i];
-}
-
-AudioBufferPool::Buffer* AudioBufferPool::acquireBuffer (int numChannels, int numSamples)
-{
-  AudioBufferPool::Buffer* buffer = 0;
-  int samplesNeeded = numChannels * numSamples;
-
-  int index = -1;
-
-  {
-    m_mutex.enter ();
-
-    for (int i = 0; i < m_buffers.size(); ++i)
-    {
-      Buffer* cur = m_buffers[i];
-
-      if (!buffer)
-      {
-        buffer = cur;
-        index = i;
-      }
-      else
-      {
-        int numSamplesAvailable = cur->getNumSamplesAllocated();
-
-        // Use the smallest buffer which is equal or bigger than what
-        // we need. If no buffer is large enough, then we will use the
-        // largest available and increase it, to minimize memory usage.
-        if ( (numSamplesAvailable >= samplesNeeded &&
-              numSamplesAvailable < buffer->getNumSamplesAllocated()) ||
-             (numSamplesAvailable < samplesNeeded &&
-              numSamplesAvailable > buffer->getNumSamplesAllocated()))
-        {
-          buffer = cur;
-          index = i;
-        }
-      }
-    }
-
-    if (buffer)
-      m_buffers.remove (index);
-
-    m_mutex.exit();
-  }
-
-  if (buffer)
-    buffer->resize (numChannels, numSamples);
-  else
-    buffer = new Buffer (numChannels, numSamples);
-
-  return buffer;
-}
-
-void AudioBufferPool::releaseBuffer (Buffer* buffer)
-{
-  if (buffer)
-  {
-    m_mutex.enter ();
-
-    m_buffers.add (buffer);
-
-    m_mutex.exit ();
-  }
 }
 
 END_VF_NAMESPACE
