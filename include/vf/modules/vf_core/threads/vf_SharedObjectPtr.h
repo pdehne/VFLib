d392e6fcbec1aad68b9e5e2db7c8104a660d042b
Vinnie Falco <vinnie.falco@gmail.com>
2012-03-12 14:44:29 -0700

    update SharedObjectPtr

--- include/vf/modules/vf_core/threads/vf_SharedObjectPtr.h
+++ include/vf/modules/vf_core/threads/vf_SharedObjectPtr.h
@@ -5,136 +5,196 @@
 #ifndef __VF_SHAREDOBJECTPTR_VFHEADER__
 #define __VF_SHAREDOBJECTPTR_VFHEADER__
 
-// RAII wrapper for a SharedObject
+// Smart pointer which points to a SharedObject.
+// Ideas from Juce.
 //
+
 template <class SharedObjectClass>
 class SharedObjectPtr
 {
 public:
-  inline SharedObjectPtr() noexcept
-    : referencedObject (0)
-  {
-  }
+    /** The class being referenced by this pointer. */
+    typedef SharedObjectClass SharedType;
 
-  inline SharedObjectPtr (SharedObjectClass* const refCountedObject) noexcept
-    : referencedObject (refCountedObject)
-  {
-    if (refCountedObject != nullptr)
-      refCountedObject->incReferenceCount();
-  }
-
-  inline SharedObjectPtr (const SharedObjectPtr <SharedObjectClass>& other) noexcept
-    : referencedObject (other.referencedObject)
-  {
-    if (referencedObject != nullptr)
-      referencedObject->incReferenceCount();
-  }
-
-  SharedObjectPtr <SharedObjectClass>& operator= (const SharedObjectPtr<SharedObjectClass>& other)
-  {
-    SharedObjectClass* const newObject = other.referencedObject;
-
-    if (newObject != referencedObject)
+    //==============================================================================
+    /** Creates a pointer to a null object. */
+    inline SharedObjectPtr() noexcept
+        : referencedObject (nullptr)
     {
-      if (newObject != nullptr)
-        newObject->incReferenceCount();
-
-      SharedObjectClass* const oldObject = referencedObject;
-      referencedObject = newObject;
-
-      if (oldObject != nullptr)
-        oldObject->decReferenceCount();
     }
 
-    return *this;
-  }
+    /** Creates a pointer to an object.
 
-  SharedObjectPtr <SharedObjectClass>& operator= (SharedObjectClass* const newObject)
-  {
-    if (referencedObject != newObject)
+        This will increment the object's reference-count if it is non-null.
+    */
+    inline SharedObjectPtr (SharedObjectClass* const refCountedObject) noexcept
+        : referencedObject (refCountedObject)
     {
-      if (newObject != nullptr)
-        newObject->incReferenceCount();
-
-      SharedObjectClass* const oldObject = referencedObject;
-      referencedObject = newObject;
-
-      if (oldObject != nullptr)
-        oldObject->decReferenceCount();
+        if (refCountedObject != nullptr)
+            refCountedObject->incReferenceCount();
     }
 
-    return *this;
-  }
+    /** Copies another pointer.
+        This will increment the object's reference-count (if it is non-null).
+    */
+    inline SharedObjectPtr (const SharedObjectPtr& other) noexcept
+        : referencedObject (other.referencedObject)
+    {
+        if (referencedObject != nullptr)
+            referencedObject->incReferenceCount();
+    }
 
-  inline ~SharedObjectPtr ()
-  {
-    if (referencedObject != nullptr)
-      referencedObject->decReferenceCount();
-  }
+   #if JUCE_COMPILER_SUPPORTS_MOVE_SEMANTICS
+    /** Takes-over the object from another pointer. */
+    inline SharedObjectPtr (SharedObjectPtr&& other) noexcept
+        : referencedObject (other.referencedObject)
+    {
+        other.referencedObject = nullptr;
+    }
+   #endif
 
-  inline operator SharedObjectClass* () const noexcept
-  {
-    return referencedObject;
-  }
+    /** Copies another pointer.
+        This will increment the object's reference-count (if it is non-null).
+    */
+    template <class DerivedClass>
+    inline SharedObjectPtr (const SharedObjectPtr<DerivedClass>& other) noexcept
+        : referencedObject (static_cast <SharedObjectClass*> (other.getObject()))
+    {
+        if (referencedObject != nullptr)
+            referencedObject->incReferenceCount();
+    }
 
-  inline SharedObjectClass* operator-> () const noexcept
-  {
-    return referencedObject;
-  }
+    /** Changes this pointer to point at a different object.
 
-  inline SharedObjectClass* getObject () const noexcept
-  {
-    return referencedObject;
-  }
+        The reference count of the old object is decremented, and it might be
+        deleted if it hits zero. The new object's count is incremented.
+    */
+    SharedObjectPtr& operator= (const SharedObjectPtr& other)
+    {
+        return operator= (other.referencedObject);
+    }
+
+    /** Changes this pointer to point at a different object.
+
+        The reference count of the old object is decremented, and it might be
+        deleted if it hits zero. The new object's count is incremented.
+    */
+    template <class DerivedClass>
+    SharedObjectPtr& operator= (const SharedObjectPtr<DerivedClass>& other)
+    {
+        return operator= (static_cast <SharedObjectClass*> (other.getObject()));
+    }
+
+   #if JUCE_COMPILER_SUPPORTS_MOVE_SEMANTICS
+    /** Takes-over the object from another pointer. */
+    SharedObjectPtr& operator= (SharedObjectPtr&& other)
+    {
+        std::swap (referencedObject, other.referencedObject);
+        return *this;
+    }
+   #endif
+
+    /** Changes this pointer to point at a different object.
+
+        The reference count of the old object is decremented, and it might be
+        deleted if it hits zero. The new object's count is incremented.
+    */
+    SharedObjectPtr& operator= (SharedObjectClass* const newObject)
+    {
+        if (referencedObject != newObject)
+        {
+            if (newObject != nullptr)
+                newObject->incReferenceCount();
+
+            SharedObjectClass* const oldObject = referencedObject;
+            referencedObject = newObject;
+
+            if (oldObject != nullptr)
+                oldObject->decReferenceCount();
+        }
+
+        return *this;
+    }
+
+    /** Destructor.
+
+        This will decrement the object's reference-count, and may delete it if it
+        gets to zero.
+    */
+    inline ~SharedObjectPtr()
+    {
+        if (referencedObject != nullptr)
+            referencedObject->decReferenceCount();
+    }
+
+    /** Returns the object that this pointer references.
+        The pointer returned may be zero, of course.
+    */
+    inline operator SharedObjectClass*() const noexcept
+    {
+        return referencedObject;
+    }
+
+    // the -> operator is called on the referenced object
+    inline SharedObjectClass* operator->() const noexcept
+    {
+        return referencedObject;
+    }
+
+    /** Returns the object that this pointer references.
+        The pointer returned may be zero, of course.
+    */
+    inline SharedObjectClass* getObject() const noexcept
+    {
+        return referencedObject;
+    }
 
 private:
-  SharedObjectClass* referencedObject;
+    //==============================================================================
+    SharedObjectClass* referencedObject;
 };
 
-//
-// Comparisons
-//
 
+/** Compares two SharedObjectPointers. */
 template <class SharedObjectClass>
-bool operator== (const SharedObjectPtr <SharedObjectClass>& object1,
-                 SharedObjectClass* const object2) noexcept
+bool operator== (const SharedObjectPtr<SharedObjectClass>& object1, SharedObjectClass* const object2) noexcept
 {
-  return object1.getObject() == object2;
+    return object1.getObject() == object2;
 }
 
+/** Compares two SharedObjectPointers. */
 template <class SharedObjectClass>
-bool operator== (const SharedObjectPtr <SharedObjectClass>& object1,
-                 const SharedObjectPtr<SharedObjectClass>& object2) noexcept
+bool operator== (const SharedObjectPtr<SharedObjectClass>& object1, const SharedObjectPtr<SharedObjectClass>& object2) noexcept
 {
-  return object1.getObject() == object2.getObject();
+    return object1.getObject() == object2.getObject();
 }
 
+/** Compares two SharedObjectPointers. */
 template <class SharedObjectClass>
-bool operator== (SharedObjectClass* object1,
-                 SharedObjectPtr <SharedObjectClass>& object2) noexcept
+bool operator== (SharedObjectClass* object1, SharedObjectPtr<SharedObjectClass>& object2) noexcept
 {
-  return object1 == object2.getObject();
+    return object1 == object2.getObject();
 }
 
+/** Compares two SharedObjectPointers. */
 template <class SharedObjectClass>
-bool operator!= (const SharedObjectPtr <SharedObjectClass>& object1,
-                 const SharedObjectClass* object2) noexcept
+bool operator!= (const SharedObjectPtr<SharedObjectClass>& object1, const SharedObjectClass* object2) noexcept
 {
-  return object1.getObject() != object2;
+    return object1.getObject() != object2;
 }
 
+/** Compares two SharedObjectPointers. */
 template <class SharedObjectClass>
-bool operator!= (const SharedObjectPtr <SharedObjectClass>& object1,
-                 SharedObjectPtr<SharedObjectClass>& object2) noexcept
+bool operator!= (const SharedObjectPtr<SharedObjectClass>& object1, SharedObjectPtr<SharedObjectClass>& object2) noexcept
 {
-  return object1.getObject() != object2.getObject();
+    return object1.getObject() != object2.getObject();
 }
 
+/** Compares two SharedObjectPointers. */
 template <class SharedObjectClass>
-bool operator!= (SharedObjectClass* object1,
-                 SharedObjectPtr <SharedObjectClass>& object2) noexcept
+bool operator!= (SharedObjectClass* object1, SharedObjectPtr<SharedObjectClass>& object2) noexcept
 {
-  return object1 != object2.getObject();
+    return object1 != object2.getObject();
 }
 
 #endif
