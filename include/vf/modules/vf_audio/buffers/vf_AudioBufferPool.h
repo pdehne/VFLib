9ed88de56dd9438b42b779011f3bc3f7b843673b
Vinnie Falco <vinnie.falco@gmail.com>
2012-03-29 23:44:57 -0700

    clean whitespace

--- include/vf/modules/vf_audio/buffers/vf_AudioBufferPool.h
+++ include/vf/modules/vf_audio/buffers/vf_AudioBufferPool.h
@@ -7,101 +7,101 @@
 class AudioBufferPoolBase
 {
 public:
-    class Buffer : public VF_JUCE::AudioSampleBuffer
-    {
-    public:
-        Buffer (int numChannels,
-                int numSamples);
-        
-        void resize (int newNumChannels, int newNumSamples);
-        
-        int getNumSamplesAllocated () const;
-        
-    private:
-        int m_samplesAllocated;
-    };
+  class Buffer : public VF_JUCE::AudioSampleBuffer
+  {
+  public:
+	Buffer (int numChannels,
+	  int numSamples);
+
+	void resize (int newNumChannels, int newNumSamples);
+
+	int getNumSamplesAllocated () const;
+
+  private:
+	int m_samplesAllocated;
+  };
 };
 
 // Manages a collection of AudioSampleBuffer that
 // persist in memory and never decrease in size.
 template <class BufferClass>
 class AudioBufferPoolTemplate
-: public AudioBufferPoolBase
-, public LeakChecked <AudioBufferPoolTemplate<BufferClass> >
+  : public AudioBufferPoolBase
+  , public LeakChecked <AudioBufferPoolTemplate<BufferClass> >
 {
 public:
   AudioBufferPoolTemplate ()
   {
-    m_buffers.ensureStorageAllocated (10);
+	m_buffers.ensureStorageAllocated (10);
   }
-    
+
   ~AudioBufferPoolTemplate ()
   {
-    for (int i = 0; i < m_buffers.size(); ++i)
-      delete m_buffers[i];
+	for (int i = 0; i < m_buffers.size(); ++i)
+	  delete m_buffers[i];
   }    
 
   BufferClass* acquireBuffer (int numChannels, int numSamples)
   {
-    BufferClass* buffer = 0;
-    int samplesNeeded = numChannels * numSamples;
-    
-    int index = -1;
-    
-    {
-        m_mutex.enter ();
-        
-        for (int i = 0; i < m_buffers.size(); ++i)
-        {
-            BufferClass* cur = m_buffers[i];
-            
-            if (!buffer)
-            {
-                buffer = cur;
-                index = i;
-            }
-            else
-            {
-                int numSamplesAvailable = cur->getNumSamplesAllocated();
-                
-                // Use the smallest buffer which is equal or bigger than what
-                // we need. If no buffer is large enough, then we will use the
-                // largest available and increase it, to minimize memory usage.
-                if ( (numSamplesAvailable >= samplesNeeded &&
-                      numSamplesAvailable < buffer->getNumSamplesAllocated()) ||
-                    (numSamplesAvailable < samplesNeeded &&
-                     numSamplesAvailable > buffer->getNumSamplesAllocated()))
-                {
-                    buffer = cur;
-                    index = i;
-                }
-            }
-        }
-        
-        if (buffer)
-            m_buffers.remove (index);
-        
-        m_mutex.exit();
-    }
-    
-    if (buffer)
-        buffer->resize (numChannels, numSamples);
-    else
-        buffer = new BufferClass (numChannels, numSamples);
-    
-    return buffer;
+	BufferClass* buffer = 0;
+	int samplesNeeded = numChannels * numSamples;
+
+	int index = -1;
+
+	{
+	  m_mutex.enter ();
+
+	  for (int i = 0; i < m_buffers.size(); ++i)
+	  {
+		BufferClass* cur = m_buffers[i];
+
+		if (!buffer)
+		{
+		  buffer = cur;
+		  index = i;
+		}
+		else
+		{
+		  int numSamplesAvailable = cur->getNumSamplesAllocated();
+
+		  // Use the smallest buffer which is equal or bigger than what
+		  // we need. If no buffer is large enough, then we will use the
+		  // largest available and increase it, to minimize memory usage.
+		  if ( (numSamplesAvailable >= samplesNeeded &&
+			numSamplesAvailable < buffer->getNumSamplesAllocated()) ||
+			(numSamplesAvailable < samplesNeeded &&
+			numSamplesAvailable > buffer->getNumSamplesAllocated()))
+		  {
+			buffer = cur;
+			index = i;
+		  }
+		}
+	  }
+
+	  if (buffer)
+		m_buffers.remove (index);
+
+	  m_mutex.exit();
+	}
+
+	if (buffer)
+	  buffer->resize (numChannels, numSamples);
+	else
+	  buffer = new BufferClass (numChannels, numSamples);
+
+	return buffer;
   }
 
   void releaseBuffer (BufferClass* buffer)
   {
-    if (buffer)
-    {
-        m_mutex.enter ();
-        
-        m_buffers.add (buffer);
-        
-        m_mutex.exit ();
-    }
+	if (buffer)
+	{
+	  m_mutex.enter ();
+
+	  m_buffers.add (buffer);
+
+	  m_mutex.exit ();
+	}
   }    
 
 private:
@@ -121,36 +121,36 @@ class ScopedAudioSampleBuffer
 {
 public:
   ScopedAudioSampleBuffer (AudioBufferPool& pool,
-                           int numChannels,
-                           int numSamples)
-      : m_pool (pool)
-      , m_buffer (pool.acquireBuffer (numChannels, numSamples))
+	int numChannels,
+	int numSamples)
+	: m_pool (pool)
+	, m_buffer (pool.acquireBuffer (numChannels, numSamples))
   {
   }
-  
+
   ~ScopedAudioSampleBuffer ()
   {
-    m_pool.releaseBuffer (m_buffer);
+	m_pool.releaseBuffer (m_buffer);
   }
 
   VF_JUCE::AudioSampleBuffer* getBuffer ()
   {
-    return m_buffer;
+	return m_buffer;
   }
 
   VF_JUCE::AudioSampleBuffer* operator-> ()
   {
-    return getBuffer();
+	return getBuffer();
   }
 
   VF_JUCE::AudioSampleBuffer& operator* ()
   {
-    return *getBuffer();
+	return *getBuffer();
   }
 
   operator VF_JUCE::AudioSampleBuffer* ()
   {
-    return getBuffer();
+	return getBuffer();
   }
 
 private:
@@ -165,34 +165,34 @@ class ScopedAudioSampleBuffer
 {
 public:
   ScopedAudioSampleBuffer (AudioBufferPool& pool,
-                           int numChannels,
-                           int numSamples)
-      : m_buffer (numChannels, numSamples)
+	int numChannels,
+	int numSamples)
+	: m_buffer (numChannels, numSamples)
   {
   }
-  
+
   ~ScopedAudioSampleBuffer ()
   {
   }
 
   VF_JUCE::AudioSampleBuffer* getBuffer ()
   {
-    return &m_buffer;
+	return &m_buffer;
   }
 
   VF_JUCE::AudioSampleBuffer* operator-> ()
   {
-    return getBuffer();
+	return getBuffer();
   }
 
   VF_JUCE::AudioSampleBuffer& operator* ()
   {
-    return *getBuffer();
+	return *getBuffer();
   }
 
   operator VF_JUCE::AudioSampleBuffer* ()
   {
-    return getBuffer();
+	return getBuffer();
   }
 
 private:
