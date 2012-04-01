b0dd8a379e4363ccd32a9ec44b4540d4324613c6
Vinnie Falco <vinnie.falco@gmail.com>
2012-04-01 11:41:44 -0700

    add comments

--- include/vf/modules/vf_concurrent/queue/vf_ManualCallQueue.h
+++ include/vf/modules/vf_concurrent/queue/vf_ManualCallQueue.h
@@ -6,10 +6,12 @@
 
 #include "vf_CallQueue.h"
 
-//
-// A CallQueue that requires periodic manual calls to process.
-//
+/****
+  A CallQueue that requires periodic manual calls to process.
 
+  This is useful for adding a call queue to things like the
+  audioIODeviceCallback, which provides its own thread.
+*/
 class ManualCallQueue : public CallQueue
 {
 public:
