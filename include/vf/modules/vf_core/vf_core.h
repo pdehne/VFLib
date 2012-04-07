04b1388ad810c4d5e8cd98c0d821d31eecda18a6
Vinnie Falco <vinnie.falco@gmail.com>
2012-04-07 15:56:13 -0700

    disable a warning

--- include/vf/modules/vf_core/vf_core.h
+++ include/vf/modules/vf_core/vf_core.h
@@ -35,7 +35,17 @@ namespace vf
 #include "memory/vf_AtomicFlag.h"
 #include "memory/vf_AtomicPointer.h"
 #include "memory/vf_AtomicState.h"
+
+#if JUCE_MSVC
+#pragma warning (push)
+#pragma warning (disable: 4100) // unreferenced formal parmaeter
+#pragma warning (disable: 4355) // 'this' used in base member
+#endif
 #include "memory/vf_CacheLine.h"
+#if JUCE_MSVC
+#pragma warning (pop)
+#endif
+
 #include "memory/vf_MemoryAlignment.h"
 #include "memory/vf_ReferenceCountedSingleton.h"
 #include "memory/vf_StaticObject.h"
