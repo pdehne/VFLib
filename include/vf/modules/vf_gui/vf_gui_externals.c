755b475f40fdb9bc21c87a3f3bad9ff085eb1037
Vinnie Falco <vinnie.falco@gmail.com>
2012-04-08 10:07:33 -0700

    add FreeTypeAmalgam external

---
+++ include/vf/modules/vf_gui/vf_gui_externals.c
@@ -0,0 +1,10 @@
+// Copyright (C) 2008 by Vinnie Falco, this file is part of VFLib.
+// See the file LICENSE.txt for licensing information.
+
+#include "AppConfig.h"
+
+#if VF_USE_FREETYPE
+
+#include "../../externals/freetype/FreeTypeAmalgam.c"
+
+#endif
---
+++ include/vf/modules/vf_gui/vf_gui_externals.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2008 by Vinnie Falco, this file is part of VFLib.
+// See the file LICENSE.txt for licensing information.
+
+#ifndef VF_GUI_EXTERNALS_VFHEADER
+#define VF_GUI_EXTERNALS_VFHEADER
+
+#include "AppConfig.h"
+
+#if VF_USE_FREETYPE
+
+#include "../../externals/freetype/FreeTypeAmalgam.h"
+
+#endif
+
+#endif
