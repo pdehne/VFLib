d3fdbb7d05a08d255357982c8d200b0714372b37
Vinnie Falco <vinnie.falco@gmail.com>
2012-04-09 11:08:36 -0700

    docs for ComponentBroadcast

--- include/vf/modules/vf_gui/components/vf_ComponentBroadcast.h
+++ include/vf/modules/vf_gui/components/vf_ComponentBroadcast.h
@@ -22,41 +22,134 @@
 #ifndef __VF_COMPONENTBROADCAST_VFHEADER__
 #define __VF_COMPONENTBROADCAST_VFHEADER__
 
-// Calling this functor will recursively traverse a Component and all of its
-// children, and call a member function for each Component that exposes the
-// interface (determined via dynamic_cast).
-//
-// This requires vf::bind, std::tr1::bind, or std::bind to work.
-//
-/* Usage example
+//==============================================================================
+/** Broadcast to a Component and its children, recursively.
 
-  struct PersistentObject
-  {
-    virtual void onSaveSettings (XmlElement* xml) = 0;
-    virtual void onLoadSettings (XmlElement* xml) = 0;
-  };
+    This class functor will traverse a Component and its children recursively
+    and call a member function for each Component that exposes the desired
+    interface. A Component exposes the interface by deriving from the class
+    containing the member function of interest. The implementation uses
+    dynamic_cast to determine if the Component is eligible.
 
-  void saveAllWindowSettings (XmlElement* xml)
-  {
-    for (int i = 0; i < Desktop::getInstance().getNumComponents(); ++i)
+    This provides robust assistance for enforcing separation of concerns, and
+    decentralizing program logic into only the areas that need it.
+
+    In this example we will define an interface for saving and restoring
+    window settings into an XmlElement, declare a Component that supports
+    the interface, and finally provide a function that calls all windows
+    to save their settings:
+
+    @code
+
+    // Interface for saving and loading Component settings across launches
+    //
+    struct SerializableComponent
     {
-      Component* c = Desktop::getInstance().getComponent (i);
-    
-      // Any Component of any open window that derives from PersistentObject
-      // will have its onSaveSettings() member called by this broadcast.
-      //
-      componentBroadcast (c, &PersistentObject::onSaveSettings, xml);
-    }
-  }
-*/
+      virtual void onSaveSettings (XmlElement* xml) { }
+      virtual void onLoadSettings (XmlElement* xml) { }
+    };
 
+    // Declare our component
+    //
+    class PersistentComponent : public SerializableComponent
+    {
+    public:
+      void onSaveSettings (XmlElement* xmlTree)
+      {
+        // Save our state into xmlTree
+      }
+      
+      void onLoadSettings (XmlElement* xmlTree)
+      {
+        // Retrieve our saved state from xmlTree if it exists
+      }
+    };
+
+    // This will tell every component in every window to save the settings
+    // if it supports the interface.
+    //
+    void saveAllWindowSettings (XmlElement* xmlTree)
+    {
+      // Go through all application windows on the desktop.
+      //
+      for (int i = 0; i < Desktop::getInstance().getNumComponents(); ++i)
+      {
+        Component* c = Desktop::getInstance().getComponent (i);
+
+        // Call onSaveSettings for every Component that supports the
+        // interface, on the window and all its children, recursively.
+        //
+        componentBroadcast (c, &PersistentComponent::onSaveSettings, xmlTree);
+      }
+    }
+
+    // Create a ResizableWindow and tell it to load it's settings
+    //
+    void createWindowFromSettings (XmlElement* xmlTree)
+    {
+      // First create the window
+      //
+      ResizableWindow* w = new ResizableWindow (...);
+
+      // Now call onLoadSettings for every Component in the window
+      // that supports the interface, recursively.
+      //
+      componentBroadcast (w, &PersistentComponent::onLoadSettings, xmlTree);
+    }
+
+    @endcode
+
+    componentBroadcast can even be used to call member functions of
+    already existing interfaces, without changing any Components! This
+    example will automatically clear the contents of every child TextEditor
+    Component in a DocumentWindow:
+
+    @code
+
+    void clearAllTextEditors (DocumentWindow* window)
+    {
+      componentBroadcast (window, &TextEditor::clear);
+    }
+
+    @endcode
+
+    Wow!
+
+    These are some of the benefits of using this system:
+
+    - Code that broadcasts to the interface is generic. It doesn't need to
+      know who is being called.
+
+    - Components implementing the interface don't need to know about who
+      is performing the broadcast.
+
+    - Components don't care where they are in the TopLevelWindow's hierarchy,
+      they can be added, removed, or reparented without affecting a broadcaster
+      or other recipients.
+
+    - If an individual child Component does not support the interface,
+      it is simply skipped. This makes it easy to add broadcasting features
+      to already written Component hierarchies.
+
+    - Broadcast interfaces can be broken up into any number of individual
+      classes, they don't need to know about each other. A Component can
+      expose zero, one, or any number of interfaces to receive broadcasts.
+*/
 class componentBroadcast
 {
 public:
+  /** Call a member function on a Component and all of it's children.
+
+      Overloads are provided for calling members with up to 8 arguments
+
+      @param c  The Component to broadcast to.
+      @param f  Any non-static class member to call.
+  */
   template <class C>
   componentBroadcast (Component* c, void (C::*f)())
   { call <C> (c, vf::bind (f, vf::_1)); }
 
+#ifndef DOXYGEN
   template <class C, class T1>
   componentBroadcast (Component* c, void (C::*f)(T1), T1 t1)
   { call <C> (c, vf::bind (f, vf::_1, t1)); }
@@ -91,6 +184,7 @@ public:
   componentBroadcast (Component* c, void (C::*f)(T1, T2, T3, T4, T5, T6, T7, T8),
              T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8)
   { call <C> (c, vf::bind (f, vf::_1, t1, t2, t3, t4, t5, t6, t7, t8)); }
+#endif
 
 private:
   template <class Interface, class Functor>
