1a69e296843aece10e3ca41a128d0b27b87048c7
Vinnie Falco <vinnie.falco@gmail.com>
2012-06-21 09:49:57 -0700

    Update docs for InterruptibleThread

--- modules/vf_core/threads/vf_InterruptibleThread.h
+++ modules/vf_core/threads/vf_InterruptibleThread.h
@@ -97,40 +97,89 @@ public:
   typedef Thread::ThreadID id;
 
 public:
+  /**
+    Construct an interruptible thread.
+
+    The name is used for debugger diagnostics.
+
+    @param name The name of the thread.
+  */
   explicit InterruptibleThread (String name);
+
+  /**
+    Destroy the interruptible thread.
+
+    This will signal an interrupt and wait until the thread exits.
+  */
   ~InterruptibleThread ();
 
+  /**
+    Start the thread.
+  */
   void start (Function <void (void)> const& f);
 
+  /**
+    Wait for the thread to exit.
+  */
   void join ();
 
-  // Blocks until an interrupt occurs or the timeout expires.
-  // If milliseconds is less than 0, the wait is infinite.
-  // May only be called by the thread of execution.
-  // Returns true if the interrupt occurred, or false if
-  // the timeout expired.
-  //
+  /**
+    Wait for interrupt or timeout.
+
+    This call blocks until the thread is interrupted, or until the timeout
+    expires if milliSeconds is non-negative.
+
+    May only be called by the thread of execution.
+
+    @param milliSeconds The amount of time to wait.
+                        Negative values mean no timeout.
+                        
+    @return \c true if the interrupt occurred, or \c false if the timeout expired.
+  */
   bool wait (int milliSeconds = -1);
 
-  // Interrupts the thread.
-  //
+  /**
+    Interrupt the thread of execution.
+
+    This can be called from any thread.
+  */
   void interrupt ();
 
-  // Called by the thread function, returns an Interrupted
-  // object indicating whether or not interruption is requested.
-  //
+  /**
+    Determine if an interruption is requested.
+
+    After the function returns \c true, the interrupt status is cleared.
+    Subsequent calls will return \c false until another interrupt is requested.
+
+    May only be called by the thread of execution.
+
+    @return \c true if an interrupt was requested.
+  */
   Interrupted interruptionPoint ();
 
+  /**
+    Get the ID of the associated thread.
+
+    @return The ID of the thread.
+  */
   id getId () const;
 
-  // Returns true if the caller is this thread of execution.
-  // Only valid if the thread has been started.
-  //
+  /**
+    Determine if this is the thread of execution.
+
+    @note The return value is undefined if the thread is not running.
+
+    @return \c true if the caller is this thread of execution.
+  */
   bool isTheCurrentThread () const;
 
-  // Adjusts the priority, range is 0...10.
-  // Only available in some implementations.
-  //
+  /**
+    Adjust the thread priority.
+
+    @note This only affects some platforms.
+
+    @param priority A number from 0..10
+  */
   void setPriority (int priority);
 
 private:
